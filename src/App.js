e50803dbf60d9fff7cb07100b4b91a3cc53fe79b
Eduardo Calachua <jocdwardo25@gmail.com>
2020-08-07 19:25:39 -0500

    api 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 import './App.css';
 
+const Pokemones = (props) => {
+  return(
+    <div>
+      <div>{props.nombre}</div>
+    </div>
+  )
+}
 function App() {
+  const [list, setList] = useState([])
+  const getPokemones = () => {
+    axios.get('https://pokeapi.co/api/v2/pokemon')
+      .then(function (response) {
+        // handle success
+        setList(response.data.results)
+        console.log(response);
+      })
+  }
+
+  useEffect(() => {
+    getPokemones()
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     {list.map((e)=>(<Pokemones poke={e.imagen} nombre={e.name} vida={e.vida} tipo={e.tipo}/>))}
     </div>
   );
 }
